cc82b6755517a2612857fb3315a4ba2a75c8c538
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2026-03-06 19:22:33 +0100

    Přidejte soubory projektu.

---
+++ VaclavikBC/AccountController.cs
@@ -0,0 +1,71 @@
+﻿namespace VaclavikBC
+{
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AccountController : Controller
+    {
+        // provider = Google, Facebook, GitHub, Microsoft
+        public IActionResult OAuthLogin(string provider)
+        {
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = "/Account/OAuthResponse"
+            };
+
+            return Challenge(props, provider);
+        }
+
+        public async Task<IActionResult> OAuthResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync();
+
+            if (!result.Succeeded)
+                return Content("Login failed");
+
+            var claims = result.Principal.Identities.FirstOrDefault()?.Claims;
+
+            var email = claims?.FirstOrDefault(c => c.Type.Contains("email"))?.Value;
+            var name = claims?.FirstOrDefault(c => c.Type.EndsWith("name"))?.Value; //ends with, protože je zde pole nameidentifier, které to triggerovalo
+            var provider = result.Properties?.Items[".AuthScheme"];
+
+            // ✅ SAVE / LINK USER HERE
+            // DB.User.CreateOrLink(email, provider, providerUserId)
+            Console.WriteLine($"mail: {email}, name: {name},provider: {provider}");
+            foreach (var item in claims)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            //    return Content(@"
+            //    <script>
+            //        window.opener.location.reload();
+            //        window.close();
+            //    </script>
+            //", "text/html");
+            return Content("""
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <script>
+                        (function(){
+                            try {
+                                if (window.opener && !window.opener.closed) {
+                                    window.opener.postMessage('oauth-success', '*');
+                                }
+                            } catch(e){}
+
+                            setTimeout(function(){
+                                window.close();
+                            }, 3000);
+                        })();
+                    </script>
+                </head>
+                <body>
+                Login successful. You can close this window.
+                </body>
+                </html>
+                """, "text/html");
+        }
+    }
+
+}
---
+++ VaclavikBC/Program.cs
@@ -0,0 +1,78 @@
+using AspNet.Security.OAuth.Calendly;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(
+//    options =>
+//{
+//    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+//    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+//}
+CookieAuthenticationDefaults.AuthenticationScheme
+)
+    .AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+})
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+        options.Scope.Add("https://www.googleapis.com/auth/calendar.readonly");
+        options.SaveTokens = true; // Store tokens in the authentication properties
+    })
+.AddMicrosoftAccount("Microsoft", options => {
+     options.ClientId = builder.Configuration["AzureAd:ClientId"];
+    options.ClientSecret = builder.Configuration["AzureAd:ClientSecret"];
+    options.CallbackPath = "/signin-microsoft";
+ })
+.AddCalendly("Calendly", options =>
+{
+    options.ClientId = builder.Configuration["Calendly:ClientId"];
+    options.ClientSecret = builder.Configuration["Calendly:ClientSecret"];
+    options.CallbackPath = "/signin-calendly";
+});
+
+//builder.Services.AddAuthentication()
+//    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+//    .EnableTokenAcquisitionToCallDownstreamApi()
+//        .AddMicrosoftGraph(builder.Configuration.GetSection("MicrosoftGraph"))
+//        .AddInMemoryTokenCaches();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
