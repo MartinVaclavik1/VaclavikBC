15a0c3d5aadd10f36d95457c3f51aceb69da8119
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2026-04-21 13:11:41 +0200

    vytvoření user controlleru

---
+++ VaclavikBC/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VaclavikBC.Controllers
+{
+    public class UserController: Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public UserController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            var user = new IdentityUser { UserName = username };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+                return Ok("User created");
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                username, password, false, false);
+
+            if (result.Succeeded)
+                return Ok("Logged in");
+
+            return Unauthorized();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("Signed out");
+        }
+    }
+}
